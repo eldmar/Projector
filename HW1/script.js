da6489e202924f5a3aa02eda7e2d230c778fbd1b
Iaroslav Ridkous <jaroslav.ridkous@gmail.com>
2024-01-11 22:47:01 +0100

    Add files via upload

---
+++ HW1/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let a = 20;
+let b = 4;
+
+let sum = a + b;
+console.log("Sum: ", sum);
+
+let substraction = a - b;
+console.log("Substraction: ", substraction);
+
+let division = a / b;
+console.log("Division: ", division);
+
+let mult = a * b;
+console.log("Multiplication: ", mult);
+
+let exp = a ** 4;
+console.log("Exponentiation: ", exp);
