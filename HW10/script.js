9572513e28225dbcef68a44178ab19cdf5b4243e
eldmar <ridkous@gmail.com>
2024-02-26 21:05:47 +0000

    changes #1

--- HW10/script.js
+++ HW10/script.js
@@ -3,28 +3,31 @@
 // 1 завдання
 
 function durationBetweenDates(
-  date1 = "31 Dec 2012",
-  date2 = "31 Dec 2013",
+  startDate = "31 Dec 2012",
+  endDate = "31 Dec 2013",
   dimension = "days"
 ) {
-  const firstDate = new Date(date1);
-  const secondDate = new Date(date2);
-  //   const timestampDate1 = firstDate.getTime();
-  //   const timestampDate2 = secondDate.getTime();
+  const firstDate = new Date(startDate);
+  const secondDate = new Date(endDate);
+
   let duration = Math.abs(secondDate - firstDate);
-  if (dimension === "seconds") {
-    return console.log(`${duration / 1000} seconds`);
-  } else if (dimension === "minutes") {
-    return console.log(`${duration / 60000} minutes`);
-  } else if (dimension === "hours") {
-    return console.log(`${duration / 360000} hours`);
-  } else if (dimension === "days") {
-    return console.log(`${duration / 86400000} days`);
+
+  switch (dimension) {
+    case "days":
+      return `${duration / 86400000} ${dimension}`;
+    case "hours":
+      return `${duration / 360000} ${dimension}`;
+    case "minutes":
+      return `${duration / 60000} ${dimension}`;
+    case "seconds":
+      return `${duration / 1000} ${dimension}`;
+    default:
+      return "Invalid dimension";
   }
 }
 
-durationBetweenDates("02 Aug 1985", "03 Aug 1985", "seconds"); // поверне '86400 seconds'
-durationBetweenDates("31 Jan 2022", "03 Feb 2021", "days"); // поверне '362 days'
+console.log(durationBetweenDates("02 Aug 1985", "03 Aug 1985", "seconds")); // поверне '86400 seconds'
+console.log(durationBetweenDates("31 Jan 2022", "03 Feb 2021", "days")); // поверне '362 days'
 
 // 2 завдання
 
