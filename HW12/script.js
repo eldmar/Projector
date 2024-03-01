9f8ac895c6081f424a1a9d65fb42d810f005fa83
eldmar <ridkous@gmail.com>
2024-03-01 20:22:05 +0000

    changes#1

--- HW12/script.js
+++ HW12/script.js
@@ -2,9 +2,20 @@
 
 let headerNavigation = document.getElementById("headerTwo");
 console.log("headerNavigation", headerNavigation);
+
 let sectionAboutDOM = document.getElementsByClassName("firstSection");
 console.log("firstSection", sectionAboutDOM);
-let listItem = document.querySelector(".list-item");
+
+let listItems = document.querySelectorAll("ul li");
+let listItem;
+
+for (let i = 0; i < listItems.length; i++) {
+  if (listItems[i].textContent === "Пункт 5") {
+    listItem = listItems[i];
+    break;
+  }
+}
 console.log("listItem", listItem);
-let hatredLevelBlock = document.getElementsByClassName("hatredLevelBlock");
+
+let hatredLevelBlock = document.getElementsByClassName("hatredLevelBlock")[0];
 console.log("hatredLevelBlock", hatredLevelBlock);
