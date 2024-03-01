a19a76771cb558f131844fb24ccefee932b56570
eldmar <ridkous@gmail.com>
2024-03-01 17:47:44 +0000

    done

---
+++ HW12/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let headerNavigation = document.getElementById("headerTwo");
+console.log("headerNavigation", headerNavigation);
+let sectionAboutDOM = document.getElementsByClassName("firstSection");
+console.log("firstSection", sectionAboutDOM);
+let listItem = document.querySelector(".list-item");
+console.log("listItem", listItem);
+let hatredLevelBlock = document.getElementsByClassName("hatredLevelBlock");
+console.log("hatredLevelBlock", hatredLevelBlock);
