511aa51bffd03fc719ea061003266158940c96a1
eldmar <ridkous@gmail.com>
2024-03-02 15:41:21 +0000

    changes#2

--- HW12/script.js
+++ HW12/script.js
@@ -6,16 +6,8 @@ console.log("headerNavigation", headerNavigation);
 let sectionAboutDOM = document.getElementsByClassName("firstSection");
 console.log("firstSection", sectionAboutDOM);
 
-let listItems = document.querySelectorAll("ul li");
-let listItem;
-
-for (let i = 0; i < listItems.length; i++) {
-  if (listItems[i].textContent === "Пункт 5") {
-    listItem = listItems[i];
-    break;
-  }
-}
-console.log("listItem", listItem);
+let listItems = document.querySelectorAll("ul li")[4];
+console.log("listItem", listItems);
 
 let hatredLevelBlock = document.getElementsByClassName("hatredLevelBlock")[0];
 console.log("hatredLevelBlock", hatredLevelBlock);
