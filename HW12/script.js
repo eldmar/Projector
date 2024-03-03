e0fed0bfa20b1980efe66674be0c96a185b194cd
eldmar <ridkous@gmail.com>
2024-03-03 13:53:24 +0000

    changes#3

--- HW12/script.js
+++ HW12/script.js
@@ -6,7 +6,8 @@ console.log("headerNavigation", headerNavigation);
 let sectionAboutDOM = document.getElementsByClassName("firstSection");
 console.log("firstSection", sectionAboutDOM);
 
-let listItems = document.querySelectorAll("ul li")[4];
+// let listItems = document.querySelectorAll("ul li")[4];
+let listItems = document.querySelector("ul li:nth-of-type(5)");
 console.log("listItem", listItems);
 
 let hatredLevelBlock = document.getElementsByClassName("hatredLevelBlock")[0];
